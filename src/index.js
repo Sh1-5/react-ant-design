812afaf338597435d565c31ac25a6aa0ab48e4e4
hqh <1101646169@qq.com>
2022-05-10 23:58:58 +0800

    2022-05-10

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import 'antd/dist/antd.less'
 
+import 'moment/locale/zh-cn'
+
 import App from './App'
 
 createRoot(document.getElementById('root')).render(<App />)
